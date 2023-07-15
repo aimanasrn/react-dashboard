fc4ad5b8634ddf1a23c569e85d01f7ee42d60229
Muhammad Aiman bin Kanasronsham <aimanasrn@gmail.com>
2023-07-15 09:16:30 +0800

    aimanasrn_15072023v2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,14 +21,33 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
-        {icon}
-      </span>
+      />
+      {icon}
     </button>
   </TooltipComponent>
 );
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+    
+    return() => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if(screenSize <= 900){
+      setActiveMenu(false);
+    }
+    else {
+      setActiveMenu(true);
+    }
+  },[screenSize]);
+  
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
@@ -41,21 +60,21 @@ const Navbar = () => {
       <div className="flex">
         <NavButton
           title="Cart"
-          // customFunc={() => handleClick('cart')}
+          customFunc={() => handleClick('cart')}
           color="blue"
           icon={<FiShoppingCart />}
         />
         <NavButton
           title="Chat"
           dotColor="#03C9D7"
-          // customFunc={() => handleClick('chat')}
+          customFunc={() => handleClick('chat')}
           color="blue"
           icon={<BsChatLeft />}
         />
         <NavButton
           title="Notification"
           dotColor="#03C9D7"
-          // customFunc={() => handleClick('notification')}
+          customFunc={() => handleClick('notification')}
           color="blue"
           icon={<RiNotification3Line />}
         />
@@ -65,8 +84,17 @@ const Navbar = () => {
         >
           <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => {}} >
             <img className="rounded-full w-8 h-8" src={avatar} alt="" />
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span>{' '}<span className="text-gray-400 font-bold ml-1 text-14">Aimanasrn</span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
+
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   );
